37ff52acb313a7e78e658bb9ee485f41c9b4ffd3
Chaos <hixcwork@163.com>
2019-01-31 00:47:03 +0000

    random rewrite

--- test_cases_generator.py
+++ test_cases_generator.py
@@ -8,9 +8,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 
-def test_cases_generator(min_node_number=3):
+def test_cases_generator(min_node_number=5, edge_types=3, probability=0.1):
     n = min_node_number
-    G1 = nx.fast_gnp_random_graph(n,0.5,directed=True)
+    G1 = random_labeled_digraph(n, edge_types, probability)
+
     # TODO: generate test case base on a random digraph
 
 def random_labeled_digraph(n, m, p):
@@ -20,33 +21,30 @@ def random_labeled_digraph(n, m, p):
     https://github.com/networkx/networkx/blob/master/networkx/generators/random_graphs.py
 
     :param n: number of node
-    :param m: number of edges type
+    :param m: number of edges type (a, b, c, ...)
     :param p: probability for edge creation
     :return: graph G
     '''
-
-    G = nx.DiGraph(nx.empty_graph(n))
-    # Nodes in graph are from 0,n-1 (start with v as the first node index).
-    v = 0
-    w = -1
-    lp = math.log(1.0 - p)
-    while v < n:
-        lr = math.log(1.0 - random.random())
-        w = w + 1 + int(lr / lp)
-        if v == w:  # avoid self loops
-            w = w + 1
-        while v < n <= w:
-            w = w - n
-            v = v + 1
-            if v == w:  # avoid self loops
-                w = w + 1
-        if v < n:
-            G.add_edge(v, w, action=chr(random.randint(97,96+m)))  # add a random action in range
+    G = nx.MultiDiGraph()
+    G.add_nodes_from(range(n))
+    for v in range(n):
+        for w in range(n):
+            for e in range(m):
+                if random.random() < p:
+                    G.add_edge(v, w, label=chr(97+e))
 
     return G
 
 if __name__ == '__main__':
-    G = random_labeled_digraph(5,3,0.7)
+    import pydot
+    from graphviz import render
+    from networkx.drawing.nx_pydot import write_dot
+    G = random_labeled_digraph(5,3,0.1)
+    write_dot(G, "test.dot")
+    render('dot', 'png', 'test.dot')
+
+
+
     # G = nx.DiGraph()
     # G.add_edge(1, 1, action='a')
     # G.add_edge(1, 2, action='a')
@@ -54,8 +52,8 @@ if __name__ == '__main__':
     # G.add_edge(3, 1, action='c')
     # G.add_edge(3, 2, action='e')
 
-    pos  = nx.spring_layout(G)
-    nx.draw(G, pos)
-    edge_labels = nx.get_edge_attributes(G, "action")
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.3)
-    plt.show()
+    # pos  = nx.spring_layout(G)
+    # nx.draw(G, pos)
+    # edge_labels = nx.get_edge_attributes(G, "action")
+    # nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.3)
+    # plt.show()
